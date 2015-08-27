bb8e7289cd3858c6a09d3c03d29c1344951821ed
johuynh <jhnsnhuynh05@yahoo.com>
2015-08-27 16:21:11 +0000

    Finish up the second section

---
+++ GeometryLab.java
@@ -0,0 +1,24 @@
+public class GeometryLab {
+    public double f(double x, double y) {
+        return x + y;
+        }
+    public double g(double x, double y) {
+        return x - y;
+        }
+    public double h(double x, double y) {
+        return x * y;
+        }
+    public double j(double x, double y) {
+        return x / y;
+        }
+    public double k(double x, double y) {
+        return x % y;
+        }
+    public double m(double x, double y, double z) {
+        return x + y - z;
+        }
+        public double triArea(double b, double h) {
+            return 1/2 * b * h;
+        }
+    
+} 
